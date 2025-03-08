3e12c120bafbe9da70ed84492846d4a0fc831009
Yago Michel <santiagomichelh@gmail.com>
2025-03-07 20:30:33 -0600

    Controladores y html

--- src/main/java/com/yago/formulario/controllers/MascotasController.java
+++ src/main/java/com/yago/formulario/controllers/MascotasController.java
@@ -4,12 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
-
-
-
-
-
-
 @Controller
 public class MascotasController {
     @GetMapping("/mascotas")
@@ -17,25 +11,40 @@ public class MascotasController {
         return "mascotas/mascotas-listado";
     }
     @GetMapping("/mascotas/actualizar")
-    public String actualizarMascota() {
+    public String form_actualizarMascota() {
         return "mascotas/mascotas-actualizar";
     }
     
     @GetMapping("/mascotas/eliminar")
-    public String eliminarMascota() {
+    public String form_eliminarMascota() {
         return "mascotas/mascotas-eliminar";
     }
-    
-    
 
     @GetMapping("/mascotas/agregar")
     public String Mascota() {
         return "mascotas/mascotas-agregar";
     }
+
+    //Controladores post
+
     @PostMapping("/mascotas/guardar")
     public String agregarMascota() {
-        return "mascotas/mascotas-agregar";
+
+        return "redirect:/mascotas";
     }
+
+    @PostMapping("/mascotas/eliminar")
+    public String eliminarMascota() {
+
+        return "redirect:/mascotas";
+    }
+
+    @PostMapping("/mascotas/actualizar")
+    public String actualizarMascota() {
+
+        return "redirect:/mascotas";
+    }
+
     
     
 }
